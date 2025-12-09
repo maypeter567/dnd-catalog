fa211ee03d16c74d75e9c39ea201411339303333
mdf53 <mdf53@byu.edu>
2025-12-09 13:11:40 -0700

    Worked on css stuff

--- src/pages/classes.tsx
+++ src/pages/classes.tsx
@@ -2,18 +2,18 @@ import Card from "../components/Card";
 import { defaultClasses } from "../data/Cards";
 
 export default function Classes() {
-  const cards = defaultClasses
-    .filter((item) => item)
-    .map((item, index) => (
-      <li key={index} className="card">
-        <details>
-          <summary>
-            <h3 className="card-title">{item.title}</h3>
-          </summary>
-          <p>{item.description}</p>
-        </details>
-      </li>
-    ));
+  // const cards = defaultClasses
+  //   .filter((item) => item)
+  //   .map((item, index) => (
+  //     <li key={index} className="card">
+  //       <details>
+  //         <summary>
+  //           <h3 className="card-title">{item.title}</h3>
+  //         </summary>
+  //         <p>{item.description}</p>
+  //       </details>
+  //     </li>
+  //   ));
 
   return (
     <div className="page-section">
